89a7de37e3ce83d60036dc410ed89ac70721f930
quiquelhappy <quiquelhappy@gmail.com>
2023-07-21 19:09:45 +0200

    more default values

--- src/routes/checkout/payment/+page.svelte
+++ src/routes/checkout/payment/+page.svelte
@@ -1,27 +1,15 @@
-<script>
+<script lang="ts">
     import Checkout from "$lib/elements/checkout/Checkout.svelte";
     import { Amount, Method, Checkout as checkout } from "$lib/types";
+
+    let resource: checkout | null = null;
+    (async () => {
+        resource = await checkout.fromSecret("test");
+    })();
 </script>
 
 <Checkout
-    checkout={new checkout(
-        "serverbench.io",
-        {
-            stripe: new Method("stripe", "-", "-", "payment", "pi_3NVyJ3CA7uvn961o03OQUtd0_secret_IgIq5ufxHKjmCJ8b9lR4r4qpb"),
-            paypal: new Method("paypal", "-", "-", "payment", "pi_3NVyJ3CA7uvn961o03OQUtd0_secret_IgIq5ufxHKjmCJ8b9lR4r4qpb"),
-        },
-        new Amount("eur", 25, 21, "IVA"),
-        {
-            city: "Vallirana",
-            country: "ES",
-            line1: "",
-            line2: "",
-            postal_code: "",
-            state: "Barcelona",
-        },
-        7,
-        new Amount("eur", 10, 21, "IVA", "month")
-    )}
+    checkout={resource}
     display={{
         dark: true,
         popup: false,
