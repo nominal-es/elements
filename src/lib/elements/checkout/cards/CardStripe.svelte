7babc07d0e8da8babea4b84315cc96035ee0fe57
quiquelhappy <quiquelhappy@gmail.com>
2023-07-21 11:58:06 +0200

    skeleton loader for stripe payment card

--- src/lib/elements/checkout/cards/CardStripe.svelte
+++ src/lib/elements/checkout/cards/CardStripe.svelte
@@ -4,45 +4,69 @@
 
     import Button from "$components/ui/button/Button.svelte";
     import type { Checkout, display } from "$lib/types";
-    import { onMount } from "svelte";
+    import { createEventDispatcher, onMount, onDestroy } from "svelte";
+    import Skeleton from "$components/ui/skeleton/Skeleton.svelte";
+    import type { StripePaymentElement } from "@stripe/stripe-js";
 
     export let checkout: Checkout;
     export let display: display;
 
-    onMount(async () => {
-        const elements = await checkout.methods.stripe.instance(display);
+    const dispatch = createEventDispatcher();
 
-        const payment = elements!.create("payment", {
-            layout: "tabs",
-            fields: {
-                billingDetails: {
-                    name: "never",
-                    email: "never",
-                    phone: "auto",
-                    address: "never",
+    let loading: boolean = true;
+
+    let payment: StripePaymentElement;
+
+    onMount(async () => {
+        try {
+            const elements = await checkout.methods.stripe.instance(display);
+            payment = elements!.create("payment", {
+                layout: "tabs",
+                fields: {
+                    billingDetails: {
+                        name: "never",
+                        email: "never",
+                        phone: "auto",
+                        address: "never",
+                    },
                 },
-            },
-        });
-        payment.on("change", () => {
-            // remove error
-        });
-        payment.mount("#stripe-payment-element");
+            });
+            payment.on("change", () => {
+                // remove error
+            });
+            payment.mount("#stripe-payment-element");
+            payment.on("loaderstart", () => {
+                loading = false;
+            });
+        } catch (error) {
+            loading = false;
+            dispatch("error", error);
+        }
+    });
+
+    onDestroy(() => {
+        if (payment) payment.destroy();
     });
 </script>
 
-<Card>
-    <CardHeader id="stripe-payment-element" />
-    <CardFooter>
-        <Button size="lg" class="w-full capitalize">
-            {#if checkout.upfront.amount}
-                {#if checkout.upcoming?.frequency}
-                    {$_("checkout.payAndSubscribe")}
-                {:else}
-                    {$_("checkout.pay")}
+{#if loading}
+    <Skeleton class="h-[350px]" />
+{/if}
+<section class:hidden={loading}>
+    <Card>
+        <CardHeader id="stripe-payment-element" />
+        <CardFooter>
+            <Button size="lg" class="w-full capitalize">
+                {#if checkout.upfront.amount}
+                    {#if checkout.upcoming?.frequency}
+                        {$_("checkout.payAndSubscribe")}
+                    {:else}
+                        {$_("checkout.pay")}
+                    {/if}
+                {:else if checkout.upcoming?.frequency}
+                    {$_("checkout.subscribe")}
                 {/if}
-            {:else if checkout.upcoming?.frequency}
-                {$_("checkout.subscribe")}
-            {/if}
-        </Button>
-    </CardFooter>
-</Card>
+            </Button>
+        </CardFooter>
+    </Card>
+</section>
