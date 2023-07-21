96b2af83e9bac5155961b3f1f5c29868d9906087
quiquelhappy <quiquelhappy@gmail.com>
2023-07-21 19:10:26 +0200

    checkout request

--- src/lib/elements/checkout/Checkout.svelte
+++ src/lib/elements/checkout/Checkout.svelte
@@ -8,7 +8,7 @@
     import VerifiedBadge from "../misc/VerifiedBadge.svelte";
     import ReadableAmount from "../misc/ReadableAmount.svelte";
 
-    export let checkout: Checkout;
+    export let checkout: Checkout | null;
     export let display: display;
 
     let error: Error | null = null;
@@ -19,104 +19,136 @@
     import ExpressStripe from "./express/ExpressStripe.svelte";
     import Alert from "$components/ui/alert/Alert.svelte";
     import AlertDescription from "$components/ui/alert/AlertDescription.svelte";
+    import Skeleton from "$components/ui/skeleton/Skeleton.svelte";
 
     function errorHandler(event: CustomEvent<Error>) {
+        console.log(event);
         if (!event.detail) {
             // ensure null type
             error = null;
         } else {
-            error = error;
+            error = event.detail;
         }
+    }
+
+    async function successHandler(event: CustomEvent<any>) {
+        await checkout!.check();
+        window.alert("payment complete");
     }
 </script>
 
 <!-- amount description -->
-<section
-    id="price"
-    class="flex flex-row gap-3 items-center text-xl {checkout.hasSetup
-        ? 'justify-center'
-        : ''}"
->
-    <div
-        class="font-extrabold text-ellipsis whitespace-nowrap overflow-hidden min-w-0"
+{#if checkout}
+    <section
+        id="price"
+        class="flex flex-row gap-3 items-center text-xl {checkout.hasSetup
+            ? 'justify-center'
+            : ''}"
     >
-        {checkout.displayName}
-    </div>
-    <!-- verified checkmark -->
-    {#if !checkout.app}
-        <VerifiedBadge />
-    {/if}
+        <div
+            class="font-extrabold text-ellipsis whitespace-nowrap overflow-hidden min-w-0"
+        >
+            {checkout.displayName}
+        </div>
+        <!-- verified checkmark -->
+        {#if !checkout.app}
+            <VerifiedBadge />
+        {/if}
 
-    <!-- no-setup -->
-    {#if !checkout.hasSetup}
-        <div class="grow" />
+        <!-- no-setup -->
+        {#if !checkout.hasSetup}
+            <div class="grow" />
 
-        <!-- trial indicator -->
-        <TrialBadge {checkout} />
+            <!-- trial indicator -->
+            <TrialBadge {checkout} />
 
-        <!-- tax rate indicator -->
-        <TaxBadge amount={checkout.upfront} />
+            <!-- tax rate indicator -->
+            <TaxBadge amount={checkout.upfront} />
 
-        <!-- amount indicator -->
-        <ReadableAmount amount={checkout.upfront} />
-    {:else}
-        <TaxBadge amount={checkout.upfront} />
-    {/if}
-</section>
+            <!-- amount indicator -->
+            <ReadableAmount amount={checkout.upfront} />
+        {:else}
+            <TaxBadge amount={checkout.upfront} />
+        {/if}
+    </section>
 
-<section>
-    {#if checkout.hasSetup && checkout.upcoming}
-        <!-- multi-charge breakdown -->
-        <section class="items-center justify-center flex flex-row gap-5">
-            <div class="grow w-full flex flex-col gap-3 items-center flex-wrap">
-                <div class="flex flex-row gap-3 items-center">
-                    <ReadableAmount amount={checkout.upfront} />
+    <section>
+        {#if checkout.hasSetup && checkout.upcoming}
+            <!-- multi-charge breakdown -->
+            <section class="items-center justify-center flex flex-row gap-5">
+                <div
+                    class="grow w-full flex flex-col gap-3 items-center flex-wrap"
+                >
+                    <div class="flex flex-row gap-3 items-center">
+                        <ReadableAmount amount={checkout.upfront} />
+                    </div>
+                    <span class="text-sm capitalize">
+                        {$_("checkout.totalDueToday")}
+                    </span>
                 </div>
-                <span class="text-sm capitalize">
-                    {$_("checkout.totalDueToday")}
-                </span>
-            </div>
-            <Plus />
-            <div class="grow w-full flex flex-col gap-3 items-center flex-wrap">
-                <ReadableAmount amount={checkout.upcoming} />
-                <span class="text-sm capitalize">
-                    {#if checkout.delay}
-                        {$_("checkout.startingIn")} {checkout.delay}
-                    {:else}
-                        {$_("checkout.startingInOne")}
-                        {checkout.upcoming?.frequency}
-                    {/if}
-                </span>
-            </div>
-        </section>
+                <Plus />
+                <div
+                    class="grow w-full flex flex-col gap-3 items-center flex-wrap"
+                >
+                    <ReadableAmount amount={checkout.upcoming} />
+                    <span class="text-sm capitalize">
+                        {#if checkout.delay}
+                            {$_("checkout.startingIn")} {checkout.delay}
+                        {:else}
+                            {$_("checkout.startingIn")}
+                            {checkout.upcoming?.frequency}
+                        {/if}
+                    </span>
+                </div>
+            </section>
+        {/if}
+    </section>
+
+    {#if error}
+        <Alert>
+            <AlertDescription>
+                {error.message}
+            </AlertDescription>
+        </Alert>
     {/if}
-</section>
 
-{#if error}
-    <Alert>
-        <AlertDescription>
-            {error.message}
-        </AlertDescription>
-    </Alert>
+    <!-- express buttons (google pay, apple pay, etc) -->
+    {#if "stripe" in checkout.methods}
+        <ExpressStripe
+            on:success={successHandler}
+            on:error={errorHandler}
+            {display}
+            {checkout}
+        />
+    {/if}
+
+    <!-- card forms -->
+    <section id="card">
+        {#if "stripe" in checkout.methods}
+            <CardStripe
+                on:success={successHandler}
+                on:error={errorHandler}
+                {display}
+                {checkout}
+            />
+        {/if}
+    </section>
+
+    <!-- external buttons -->
+    <section id="buttons">
+        {#if "paypal" in checkout.methods}
+            <BtnPayPal on:success={successHandler} {display} {checkout} />
+        {/if}
+    </section>
+
+    <!-- disclaimer -->
+    <section class="text-xs opacity-50 text-center first-letter:capitalize">
+        {$_("checkout.provider")}
+        {checkout.audience}, {$_("checkout.agreement")}
+    </section>
+{:else}
+    <Skeleton class="h-[28px]" />
+    <Skeleton class="h-[330px]" />
+    <Skeleton class="h-[55px]" />
+    <Skeleton class="h-[48px]" />
 {/if}
-
-<!-- express buttons (google pay, apple pay, etc) -->
-<section id="express">
-    <ExpressStripe {display} {checkout} />
-</section>
-
-<!-- card forms -->
-<section id="card">
-    <CardStripe on:error={errorHandler} {display} {checkout} />
-</section>
-
-<!-- external buttons -->
-<section id="buttons">
-    <BtnPayPal {display} {checkout} />
-</section>
-
-<!-- disclaimer -->
-<section class="mt-5 text-xs opacity-50 text-center first-letter:capitalize">
-    {$_("checkout.provider")}
-    {checkout.audience}, {$_("checkout.agreement")}
-</section>
