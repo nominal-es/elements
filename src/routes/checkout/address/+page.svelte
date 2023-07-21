5ac0eece929136a9a51e886ab52527e1b862722b
quiquelhappy <quiquelhappy@gmail.com>
2023-07-21 23:33:21 +0200

    checkout obj

--- src/routes/checkout/address/+page.svelte
+++ src/routes/checkout/address/+page.svelte
@@ -1,4 +1,13 @@
-<script>
+<script lang="ts">
     import BillingAddress from "$lib/elements/address/BillingAddress.svelte";
+    import { Checkout } from "$lib/types";
+    let checkout: Checkout;
+    (async () => {
+        checkout = await Checkout.fromSecret("");
+    })();
 </script>
-<BillingAddress/>
+
+<BillingAddress {checkout} display={{
+    dark:true,
+    popup:false,
+}} />
