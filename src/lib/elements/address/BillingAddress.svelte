08b9c795fb5c9934db20f45694ac644099504071
quiquelhappy <quiquelhappy@gmail.com>
2023-07-21 23:34:01 +0200

    address handling

--- src/lib/elements/address/BillingAddress.svelte
+++ src/lib/elements/address/BillingAddress.svelte
@@ -1,42 +1,50 @@
 <script lang="ts">
     import type { Stripe } from "stripe";
-    import {
-        loadStripe,
-        type StripeAddressElementOptions,
-    } from "@stripe/stripe-js";
     import StripeDisclaimer from "../misc/StripeDisclaimer.svelte";
-    import { onMount } from "svelte";
+    import { onMount, onDestroy } from "svelte";
     import { Button } from "$components/ui/button";
     import { _ } from "svelte-i18n";
+    import type { Checkout, display } from "$lib/types";
+    import type {
+        Address,
+        StripeAddressElement,
+        StripeAddressElementOptions,
+        StripeLinkAuthenticationElement,
+        StripePaymentElement,
+    } from "@stripe/stripe-js";
+    import Skeleton from "$components/ui/skeleton/Skeleton.svelte";
+    import { ChevronRight, Loader2 } from "lucide-svelte";
+    import { redirect } from "@sveltejs/kit";
+    import { goto } from "$app/navigation";
 
-    let secret: string;
-    let dark: boolean = false;
+    export let display: display;
+    export let checkout: Checkout | null;
+
     let customer: Stripe.Customer | undefined = undefined;
+    let mounted = 0;
+    let completed = {
+        email: false,
+        address: false,
+    };
 
-    onMount(async () => {
-        const stripe = await loadStripe(
-            "pk_test_51N06CaCA7uvn961o2Ic2dccaYYZekwvHsMLcHBoY4vKXG7bCe3ZR2NpAhJA7qPZDYq2TamxwDbCJUXsOVuR62o0o00wemRmGdC"
-        );
-        const elements = stripe!.elements({
-            clientSecret: secret,
-            appearance: {
-                theme: dark ? "night" : "stripe",
-                rules: {
-                    ".Tab": {
-                        boxShadow: "none",
-                    },
-                    ".Input": {
-                        boxShadow: "none",
-                    },
-                },
-                variables: {
-                    colorPrimary: "#" + (dark ? "ffffff" : "000000"),
-                    colorBackground: "#" + (dark ? "2c2e2f" : "f7f7f7"),
-                },
-            },
-        });
+    let emailElement: StripeLinkAuthenticationElement;
+    let addressElement: StripeAddressElement;
+    let dummyElement: StripePaymentElement;
 
-        const emailElement = elements.create("linkAuthentication", {
+    let address: Address;
+    let name: string;
+    let email: string;
+
+    let submitting = false;
+    $: mounting = mounted < 3;
+    $: loading = !checkout || submitting;
+    $: complete = completed.email && completed.address;
+
+    async function mount() {
+        if (!checkout) return;
+        const elements = await checkout.methods.stripe.instance(display);
+
+        emailElement = elements!.create("linkAuthentication", {
             defaultValues: customer?.email
                 ? {
                       email: customer?.email,
@@ -44,8 +52,13 @@
                 : undefined,
         });
         emailElement.mount("#email");
+        emailElement.on("loaderstart", () => mounted++);
+        emailElement.on("change", (ev) => {
+            email = ev.value.email;
+            completed.email = ev.complete;
+        });
 
-        const addressElement = elements.create("address", <
+        addressElement = elements!.create("address", <
             StripeAddressElementOptions
         >{
             mode: "billing",
@@ -57,8 +70,14 @@
             },
         });
         addressElement.mount("#address");
+        addressElement.on("loaderstart", () => mounted++);
+        addressElement.on("change", (ev) => {
+            address = ev.value.address;
+            name = ev.value.name;
+            completed.address = ev.complete;
+        });
 
-        const dummyElement = elements.create("payment", {
+        dummyElement = elements!.create("payment", {
             readOnly: true,
             wallets: {
                 applePay: "never",
@@ -66,7 +85,28 @@
             },
         });
         dummyElement.mount("#dummy");
+        dummyElement.on("loaderstart", () => mounted++);
+    }
+
+    async function submit() {
+        submitting = true;
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        goto("/checkout/payment");
+    }
+
+    onDestroy(() => {
+        if (dummyElement) dummyElement.destroy();
+        if (emailElement) emailElement.destroy();
+        if (addressElement) addressElement.destroy();
     });
+
+    onMount(async () => {
+        mount();
+    });
+
+    $: if (checkout) {
+        mount();
+    }
 </script>
 
 <!-- section title -->
@@ -78,16 +118,30 @@
 <div class="hidden" id="dummy" />
 
 <!-- email input, will be appended by stripe elements -->
-<div id="email" />
+<div class:hidden={loading} id="email" />
 
 <!-- address form will be appended by stripe elements -->
-<div id="address" />
+{#if mounting}
+    {#each Array(5) as _}
+        <Skeleton class="h-[43px] mt-4" />
+    {/each}
+{/if}
+<div class:hidden={mounting} id="address" />
 
 <!-- disclaimer -->
 <section class="flex flex-row gap-5 items-center">
     <StripeDisclaimer />
-    <Button class="flex flex-row gap-2" variant="outline">
-        {$_("continue")}
-        <i class="fa-solid fa-caret-right" />
+    <Button
+        disabled={!complete || loading}
+        on:click={submit}
+        class="flex flex-row gap-1 capitalize"
+        variant="default"
+    >
+        {#if !loading}
+            {$_("continue")}
+            <ChevronRight />
+        {:else}
+            <Loader2 class="animate-spin mx-10" />
+        {/if}
     </Button>
 </section>
