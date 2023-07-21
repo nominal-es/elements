5177bde0d369c1b45d828f84c3d409f18f49311e
quiquelhappy <quiquelhappy@gmail.com>
2023-07-21 13:07:57 +0200

    remove for privacy

--- src/routes/checkout/payment/+page.svelte
+++ src/routes/checkout/payment/+page.svelte
@@ -8,14 +8,15 @@
         "serverbench.io",
         {
             stripe: new Method("stripe", "-", "-", "payment", "pi_3NVyJ3CA7uvn961o03OQUtd0_secret_IgIq5ufxHKjmCJ8b9lR4r4qpb"),
+            paypal: new Method("paypal", "-", "-", "payment", "pi_3NVyJ3CA7uvn961o03OQUtd0_secret_IgIq5ufxHKjmCJ8b9lR4r4qpb"),
         },
         new Amount("eur", 25, 21, "IVA"),
         {
             city: "Vallirana",
             country: "ES",
-            line1: "Murillo 8",
-            line2: "Casa 4",
-            postal_code: "08759",
+            line1: "",
+            line2: "",
+            postal_code: "",
             state: "Barcelona",
         },
         7,
